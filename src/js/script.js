ef6c5d8eb623e01dcf9cccf6512b9224d0de90c6
Diman142 <diman142222@gmail.com>
2020-06-02 21:05:06 +0300

    add slider + some adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,63 @@
-let cons_title = document.querySelectorAll(".title");
-
-
-if(window.innerWidth<992){
-    cons_title[0].classList.remove("title_left");
-}
-
 $(document).ready(function(){
-    $('.carusel__inner').slick({
-        speed: 1000,
-        //adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                dots: true,
+
+$('.carusel__inner').slick({
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    //adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                dots: false,
                 arrows: false,
-                }
             }
-        ]
-    });
+        }
+      ]
+  });
 });
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const slider = tns({
+//     container: '.carusel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false,
+//     // responsive: {
+//     //     575: {
+
+//     //     },
+//     //     767: {
+
+//     //     },
+//     //     991: {
+
+//     //     }
+//     //   }
+//   });
+
+
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// }); 
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// }); 
