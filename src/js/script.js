88e0e30f4afccd9d722faa83058d85affe460af1
Diman142 <diman142222@gmail.com>
2020-06-03 16:12:12 +0300

    add review section + this adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+let cons_title = document.querySelectorAll(".title");
+
+
+if(window.innerWidth<992){
+    cons_title[0].classList.remove("title_left");
+}
+
+
 $('.carusel__inner').slick({
     speed: 1000,
     slidesToShow: 1,
@@ -17,6 +25,28 @@ $('.carusel__inner').slick({
         }
       ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+  
+
+  function toggleSlide(item){
+    $(item).each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
 
 
