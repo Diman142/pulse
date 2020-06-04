587c858e5a1be123479cc673d379ef3f746a88fe
Diman142 <diman142222@gmail.com>
2020-06-04 17:49:37 +0300

    add server for sending data from forms and add some animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,84 +1,123 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-let cons_title = document.querySelectorAll(".title");
-
-
-if(window.innerWidth<992){
-    cons_title[0].classList.remove("title_left");
-}
-
-// Слик слайдер
-$('.carusel__inner').slick({
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    //adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                dots: false,
-                arrows: false,
-            }
-        }
-      ]
-  });
-  // Переключение таббов
-  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-    $(this)
-      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-  });
+	let cons_title = document.querySelectorAll(".title");
 
 
-  
-  //Кнопка подробнее и назад в каталоге
-  function toggleSlide(item){
-    $(item).each(function(i){
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
-  }
+	if (window.innerWidth < 992) {
+		cons_title[0].classList.remove("title_left");
+	}
 
-  toggleSlide('.catalog-item__link');
-  toggleSlide('.catalog-item__back');
+	// Слик слайдер
+	$('.carusel__inner').slick({
+		speed: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		//adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		responsive: [{
+			breakpoint: 992,
+			settings: {
+				dots: false,
+				arrows: false,
+			}
+		}]
+	});
+	// Переключение таббов
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 
 
 
-  // открытие модального окна при нажатии кнопок консультации
-  $('[data-modal=consultation]').on('click', function() {
-    $('.overlay, #consultation').fadeIn('slow');
-  });
+	//Кнопка подробнее и назад в каталоге
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	}
 
-  //Закрытие модальных окон при нажатии крестика
-  $('.modal__close').on('click', function(){
-    $('.overlay, #consultation, #oreder, #thanks').fadeOut();
-  })
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
 
-  //Открытие моадльного окна при нажатии кнопки купить в каталоге 
-  $('.button_mini').each(function(i){
-    $(this).on('click', function(){
-        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text()); //получаем нужный заголовк товара
-        $('.overlay, #order').fadeIn('slow');
-    });
-  });
+
+	//Модальные окна
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
+
+
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: 'required',
+				phone: 'required',
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Пожалуйства введите своё имя",
+				phone: "Введите свой номер",
+				email: "Введите свой email"
+			}
+		});
+	}
+
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-9999");
+
+	$('form').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function(){
+			$(this).find('input').val('');
+
+
+			$('form').trigger('reset');
+		})
+		return false;
+	})
+
+	//smooth scroll + pageeup
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 1600){
+			$('.pageup').fadeIn();
+		}
+		else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+	new WOW().init();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
