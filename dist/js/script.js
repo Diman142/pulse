5d14a1c864ef2a2d54bb008849016ae1b010210b
Diman142 <diman142222@gmail.com>
2020-12-23 18:29:47 +0300

    refactoring

--- dist/js/script.js
+++ dist/js/script.js
@@ -113,11 +113,11 @@ $(document).ready(function () {
 		}
 	});
 
-	$("a[href=#up]").click(function(){
-		const _href = $(this).attr("href");
-		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-		return false;
-	});
+    $(".pageup").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
 	new WOW().init();
 });
